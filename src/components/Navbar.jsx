37b366da860e7c5ed19aeba8b53be50c2ace98df
Shubham <shubhamgupta43567@gmail.com>
2025-02-07 00:59:48 +0530

    btn working

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -80,7 +80,7 @@ const Navbar = () => {
           </NavLink>
 
           {/* Hamburger Menu Button (for mobile) */}
-        <div className="md:hidden fixed float-right">
+        <div className="md:hidden float-right">
           <button
             onClick={toggleMenu}
             className="text-white pl-14 focus:outline-none"

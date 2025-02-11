4f21f59b79b49fb2ba2fa849efa117e84a27d25e
Shubham Gupta <147174815+shubhamgupta2702@users.noreply.github.com>
2025-02-11 08:13:56 +0000

    footer done

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -71,7 +71,7 @@ function Footer() {
     </div>
     
     
-    <div  className="absolute inset-0 mb-0 w-full h-full bg-transparent "></div>
+    <div  className="absolute inset-0 mb-0 w-full h-full bg-transparent ">MADE BY - SHUBHAM</div>
 </footer>
       
     </>
